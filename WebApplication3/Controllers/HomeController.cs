122a70d09f8d11c853e2d4ee8e548a8ff41d9f9c
EKizevska <emilijakizevska@gmail.com>
2020-11-27 18:16:08 +0100

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<Animal> Animals { get; set; }
+
+        public HomeController()
+        {
+            Animals = new List<Animal>()
+            {
+                new Animal(1, "Emilija", 5),
+                new Animal(2, "Kristina", 7),
+                new Animal(3, "Stefan", 3),
+                new Animal(4, "Gjoko", 8)
+            };
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(Animals);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string newAnimalName, int newAnimalAge)
+        {
+            Animals.Add(new Animal(5, newAnimalName, newAnimalAge));
+            return View(Animals);
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(string animalToDelete)
+        {
+            Animal animal = Animals.FirstOrDefault(x => x.Name.Equals(animalToDelete));
+            Animals.Remove(animal);
+            return View("Index", Animals);
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Models/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Animal
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Animal(int id, string name, int age)
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+        }
+    }
+}
